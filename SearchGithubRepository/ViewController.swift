71cd2d8f50aa58372378653c2352605748f35eca
itsukiss <itsuki.program.hit@gmail.com>
2018-12-13 16:03:27 +0900

    first commit

---
+++ SearchGithubRepository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchGithubRepository
+//
+//  Created by 田中 厳貴 on 2018/12/13.
+//  Copyright © 2018年 田中 厳貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
